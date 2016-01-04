94e8d88d06235b1dceec7150231fccbfa3a2a9b6
leonard kakande <kakandeleonard@gmail.com>
2016-01-04 08:39:34 -0800

    setting up the test environment and testing the question model

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @question = questions(:question_one)    
+  end
+
+  test 'should be valid' do
+    assert @question.valid?
+  end
+
+  test 'invalid without a title' do
+    @question.title = nil
+    assert_not @question.valid?
+  end
 end
