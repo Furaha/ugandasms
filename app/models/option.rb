ea6b58ec28d50cb0b7654457ca424352c309f931
leonard kakande <kakandeleonard@gmail.com>
2016-01-05 05:06:48 -0800

    model associations

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,2 +1,5 @@
 class Option < ActiveRecord::Base
+  belongs_to :question
+  validates :title, presence: true
+  validates :question_id, presence: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
-	validates :title, presence: true
+  has_many :options
+  validates :title, presence: true
 end
