f6b31f685b6ab236b17166740f23d250fae72b5b
leonard kakande <kakandeleonard@gmail.com>
2016-01-05 04:58:25 -0800

    $ bundle exec rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104161719) do
+ActiveRecord::Schema.define(version: 20160105125639) do
+
+  create_table "options", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.datetime "created_at", null: false
