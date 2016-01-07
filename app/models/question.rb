8b3bc2599d4890d99423aeecdb0e784d7e9d805f
leonard kakande <kakandeleonard@gmail.com>
2016-01-07 00:14:23 -0800

    associating models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,6 @@
 class Question < ActiveRecord::Base
-  has_many :options
+  has_many :options, dependent: :delete_all
+  belongs_to :campaign
   validates :title, presence: true
+  validates :campaign_id, presence: true
 end
