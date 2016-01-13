a470b2ea4e517923790028beea1cff03f6bc0e29
leonard kakande <kakandeleonard@gmail.com>
2016-01-13 07:53:53 -0800

    new and create Logic

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,4 +1,24 @@
 class CampaignsController < ApplicationController
   def index
   end
+
+  def new
+  	@campaign = Campaign.new
+  end
+
+  def create
+  	@campaign = Campaign.new(campaign_params)
+  	if @campaign.save
+  		redirect_to campaigns_path
+  	else
+  		redirect_to new_campaign_path
+  	end  	
+  end
+  
+  private
+
+  def campaign_params
+  	params[:campaign][:file].content_type = MIME::Types.type_for(params[:campaign][:file].original_filename).first.content_type
+    params.require(:campaign).permit(:file, :title)
+  end
 end
