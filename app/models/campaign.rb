4bbd466e32eaf45f7ff4cf26d06b5f2708f5e479
leonard kakande <kakandeleonard@gmail.com>
2016-01-13 08:01:09 -0800

    adding the yml attachment config in the model

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,9 @@
 class Campaign < ActiveRecord::Base
+	has_attached_file :file,
+                    :path => ":rails_root/app/campaigns/:id/:filename",
+                    :url => "/campaigns/:id/:filename"
+  validates_attachment :file, presence: true,
+                              content_type: { content_type: "text/x-yaml"}
   has_many :questions, dependent: :delete_all
   validates :title, presence: true
 end
