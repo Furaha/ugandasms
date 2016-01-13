08ea9350e5e20a612cd5f11f5f6d236337c12d14
leonard kakande <kakandeleonard@gmail.com>
2016-01-13 08:16:01 -0800

    saving the campaign data

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -3,22 +3,31 @@ class CampaignsController < ApplicationController
   end
 
   def new
-  	@campaign = Campaign.new
+    @campaign = Campaign.new
   end
 
   def create
-  	@campaign = Campaign.new(campaign_params)
-  	if @campaign.save
-  		redirect_to campaigns_path
-  	else
-  		redirect_to new_campaign_path
-  	end  	
+    @campaign = Campaign.new(campaign_params)
+    if @campaign.save
+      @data = YAML.load_file(@campaign.file.path)
+      @data.each do |question|
+        question.each do |question_title, option_array|
+          @question = Question.create(campaign_id: @campaign.id, title: question_title)
+          option_array.each do |option_value|
+            Option.create(question_id: @question.id, title: option_value)
+          end
+        end
+      end
+      redirect_to campaigns_path
+    else
+      redirect_to new_campaign_path
+    end   
   end
   
   private
 
   def campaign_params
-  	params[:campaign][:file].content_type = MIME::Types.type_for(params[:campaign][:file].original_filename).first.content_type
+    params[:campaign][:file].content_type = MIME::Types.type_for(params[:campaign][:file].original_filename).first.content_type
     params.require(:campaign).permit(:file, :title)
   end
 end
