b6a27273fca23831ebd5a9faf6d5dd2267dc5a96
Godfrey Mlowe <mlowegodfrey@gmail.com>
2016-01-14 22:29:10 +0300

    adding the tests for the upload

--- test/controllers/campaigns_controller_test.rb
+++ test/controllers/campaigns_controller_test.rb
@@ -16,5 +16,26 @@ class CampaignsControllerTest < ActionController::TestCase
   		:file => Rack::Test::UploadedFile.new(Rails.root.join('test', 'fixtures', 'test.yml'), 'text/x-yaml') }
   	assert_response 302
   end
+
+  def file
+    @file ||= File.open(File.expand_path( '../../file/test.yml', __FILE__))
+  end
+
+  def uploaded_file(klass, attribute, file, content_type = 'text/yml')
+    filename = File.basename(file.path)
+    klass_label = klass.to_s.underscore
+   
+    ActionDispatch::Http::UploadedFile.new(
+      tempfile: file,
+      filename: filename,
+      head: %Q{Content-Disposition: form-data; name="#{klass_label}[#{attribute}]"; filename="#{filename}"},
+      content_type: content_type
+    )
+  end
+
+  def uploading_file
+    campaigns = Campaign.last
+    assert_equal(File.basename(file.path), campaign.file_identifier)
+  end
 end
 
