1754643c761a66958d086c3f193b48c855945f03
leonard kakande <kakandeleonard@gmail.com>
2016-01-15 07:58:32 -0800

    app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'campaigns#index'
+  match '/send_question', to: 'messages#send_questions', via: :post, as: :send_questions
+  match '/recieve_texts', to: 'messages#recieve_texts', via: :post, as: :recieve_texts
 end
