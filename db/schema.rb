0c85a639032014de5df91948247064460ce82b4a
leonard kakande <kakandeleonard@gmail.com>
2016-01-15 07:56:55 -0800

    $ bundle exec rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107075947) do
+ActiveRecord::Schema.define(version: 20160115133836) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "campaigns", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +27,12 @@ ActiveRecord::Schema.define(version: 20160107075947) do
     t.integer  "question_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
