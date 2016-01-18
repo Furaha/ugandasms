03e24821d2f0913cae7438d51fc3782e2d0a6f19
leonard kakande <kakandeleonard@gmail.com>
2016-01-18 07:14:45 -0800

    adding a message sending instance method

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,13 @@
 class Participant < ActiveRecord::Base
   validates :number, presence: true
+  def send_message(msg)
+    @twilio_number = ENV['TWILIO_NUMBER']
+    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+    message = @client.account.messages.create(
+      :from => @twilio_number,
+      :to => self.number,
+      :body => msg,
+    )
+    puts message.to
+  end
 end
