65b925b0c0e2b7e342439fe538aef8b68fa2dc0e
leonard kakande <kakandeleonard@gmail.com>
2016-01-18 07:56:10 -0800

    more processing of incoming texts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,6 @@
 class MessagesController < ApplicationController
   require 'twilio-ruby' 
-  def send_questions   
-     
-    
+  def send_questions
     @campaign = Campaign.find(params[:campaign_id])
     @question_one = @campaign.questions.first
     @options = @question_one.options
@@ -17,13 +15,40 @@ class MessagesController < ApplicationController
     @participants = Participant.all
     @participants.each do |participant|
       participant.send_message(@msg)
+      participant.track_campaign(@campaign.id, 1)
     end
+
+    redirect_to root_path
   end
 
-  def receive_texts
-    @message_body = params["Body"]
-    @from_number = params["From"]
-    @campaign = Campaign.create(title: @message_body)    
-    render nothing: true    
+  def receive_texts    
+    @phone_number = params[:From]
+    @participant = Participant.find_by(number: @phone_number)
+    process_message(@participant)
+    render nothing: true
   end
+
+  private
+
+  # Process incoming SMS
+    def process_message(participant)
+      @campaign = Campaign.find(participant.current_campaign)
+      if participant.question_count < @campaign.questions.count
+        @new_question_count = participant.question_count + 1
+        @message = @campaign.questions[@new_question_count].title
+        respond(@message)
+        participant.track_campaign(@campaign.id, @new_question_count)
+      else
+        respond("Thank You For Participating")
+        participant.track_campaign(@campaign.id, @new_question_count)
+      end
+    end
+
+    # Send an SMS back to the participant
+    def respond(message)
+      response = Twilio::TwiML::Response.new do |r|
+        r.Message message
+      end
+      render text: response.text
+    end
 end
