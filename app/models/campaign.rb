0b5be352964779dd8a96d58e08d06ec81b57b309
leonard kakande <kakandeleonard@gmail.com>
2016-01-21 05:08:17 -0800

    model associations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,5 @@
 class Campaign < ActiveRecord::Base
   has_many :questions, dependent: :delete_all
+  has_many :participant_answers, dependent: :delete_all
   validates :title, presence: true
 end
--- app/models/option.rb
+++ app/models/option.rb
@@ -1,5 +1,6 @@
 class Option < ActiveRecord::Base
   belongs_to :question
+  has_many :participant_answers, dependent: :delete_all
   validates :title, presence: true
   validates :question_id, presence: true
 end
