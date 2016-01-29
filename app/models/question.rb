46d6172cec8572782f6f03f6516ebaf17f990d86
leonard kakande <kakandeleonard@gmail.com>
2016-01-29 02:07:15 -0800

    modifying the fixture and correcting associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,5 @@
 class Question < ActiveRecord::Base
   has_many :options, dependent: :delete_all
-  has_many :answers
   belongs_to :campaign
   validates :title, presence: true
   validates :campaign_id, presence: true
