726861c1614bbe063d98e1bf8367a7ab757eebe8
leonard kakande <kakandeleonard@gmail.com>
2016-01-29 01:53:01 -0800

    the model method for getting the sms messsage

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,20 @@ class Question < ActiveRecord::Base
   belongs_to :campaign
   validates :title, presence: true
   validates :campaign_id, presence: true
+
+  def message
+    @message = "#{self.title}"
+    @options = self.options
+    if @options.nil?
+      return @message
+    else
+      @option_string = ""
+      @counter = 1
+      @options.each do |option|
+        @option_string << "\n #{@counter}. #{option.title}"
+        @counter += 1
+      end
+      return @message + @option_string
+    end
+  end
 end
