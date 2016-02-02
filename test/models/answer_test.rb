3905237d0ed033dfd4963935947f5d6f5f8c56d1
Godfrey Mlowe <mlowegodfrey@gmail.com>
2016-02-02 07:01:45 +0300

    updating the tests by fixing the failing ones due to model changes

--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -10,12 +10,12 @@ class AnswerTest < ActiveSupport::TestCase
   end
 
   test 'invalid without an option_id' do
-    @answer.option_id = nil
+    @answer.question_reply = nil
     assert_not @answer.valid?
   end
 
   test 'invalid without a question_id' do
-    @answer.question_id = nil
+    @answer.question_text = nil
     assert_not @answer.valid?
   end
 
