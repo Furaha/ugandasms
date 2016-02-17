524c86d30f5ad2404a22d48a6fee0ea535b298e3
leonard kakande <kakandeleonard@gmail.com>
2016-02-16 21:27:53 -0800

    setting up routes for the participants

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :campaigns
   match '/send_question', to: 'messages#send_questions', via: :post, as: :send_questions
   match '/receive_texts', to: 'messages#receive_texts', via: :post, as: :receive_texts
+  resources :participants
 end
