90b1cd28def8c09a2768659f011423851f7220f4
leonard kakande <kakandeleonard@gmail.com>
2016-02-16 21:28:42 -0800

    more CRUD controller actions

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,2 +1,26 @@
 class ParticipantsController < ApplicationController
+
+  def index
+    @participants = Participant.all 
+  end
+
+  def new   
+  end
+
+  def create
+    begin
+      Participant.import(params[:file])
+      redirect_to participants_path, notice: "Products imported."
+    rescue
+      redirect_to participants_path, notice: "Invalid CSV file format."
+    end
+  end
+
+  def edit
+    @participant = Participant.find(params[:id])
+  end
+
+  def update
+    @participant = Participant.find(params[:id])
+  end
 end
