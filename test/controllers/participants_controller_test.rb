e31d91250b106e182c419bf0eb4d0b2aae419b0a
leonard kakande <kakandeleonard@gmail.com>
2016-02-16 22:51:17 -0800

    testing the controller methods

--- test/controllers/participants_controller_test.rb
+++ test/controllers/participants_controller_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class ParticipantsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@participant = participants(:one)
+	end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "GET new" do
+  	get :new
+  	assert_response :success
+  end
+
+  test "GET edit" do
+  	get :edit, id: @participant.id
+  	assert_response :success
+  end
+
+  test "PATCH Update" do
+  	patch :update, id: @participant.id, participant: { number: '+255655719909090' }
+	  assert_response :redirect
+  end
+
+  test "POST create" do
+  	post :create, :file => Rack::Test::UploadedFile.new(Rails.root.join('app', 'campaigns', 'test.csv'), 'text/csv')
+  	assert_response 302
+  end
 end
