d884693c7c673b106c1e0f83ed476c8766db25ef
leonard kakande <kakandeleonard@gmail.com>
2016-02-16 23:14:18 -0800

    proper indentation

--- test/controllers/participants_controller_test.rb
+++ test/controllers/participants_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class ParticipantsControllerTest < ActionController::TestCase
-	def setup
-		@participant = participants(:one)
-	end
+  def setup
+    @participant = participants(:one)
+  end
 
   test "should get index" do
     get :index
@@ -11,22 +11,22 @@ class ParticipantsControllerTest < ActionController::TestCase
   end
 
   test "GET new" do
-  	get :new
-  	assert_response :success
+    get :new
+    assert_response :success
   end
 
   test "GET edit" do
-  	get :edit, id: @participant.id
-  	assert_response :success
+    get :edit, id: @participant.id
+    assert_response :success
   end
 
   test "PATCH Update" do
-  	patch :update, id: @participant.id, participant: { number: '+255655719909090' }
-	  assert_response :redirect
+    patch :update, id: @participant.id, participant: { number: '+255655719909090' }
+    assert_response :redirect
   end
 
   test "POST create" do
-  	post :create, :file => Rack::Test::UploadedFile.new(Rails.root.join('app', 'campaigns', 'test.csv'), 'text/csv')
-  	assert_response 302
+    post :create, :file => Rack::Test::UploadedFile.new(Rails.root.join('app', 'campaigns', 'test.csv'), 'text/csv')
+    assert_response 302
   end
 end
