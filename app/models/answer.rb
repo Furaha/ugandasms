bd49a5e38bcf511c2f93a31bb61a33ce685e086a
leonard kakande <kakandeleonard@gmail.com>
2016-02-21 21:39:55 -0800

    remove the answer files

--- app/models/answer.rb
+++
@@ -1,9 +0,0 @@
-class Answer < ActiveRecord::Base
-  belongs_to :participant
-  belongs_to :campaign
-
-  validates :participant_id, presence: true
-  validates :campaign_id, presence: true
-  validates :question_text, presence: true
-  validates :question_reply, presence: true
-end
