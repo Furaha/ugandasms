da5790ea60066a1dee780572880e574d82c0e2ff
leonard kakande <kakandeleonard@gmail.com>
2016-02-21 20:59:39 -0800

    testing the action

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -3,9 +3,15 @@ require 'test_helper'
 class MessagesControllerTest < ActionController::TestCase
   def setup
     @campaign = campaigns(:malaria)
+    @question = @campaign.questions.first
   end
   test "POST send_campaign" do
     post :start_campaign, campaign_id: @campaign.id
+    assert_response 302
+  end
+
+  test "POST send_question" do
+    post :send_question, campaign_id: @campaign.id, question_id: @question.id
     assert_response 302
   end
 end
