3925180aa3a45fd598639f0d5da777758b1a8657
leonard kakande <kakandeleonard@gmail.com>
2016-02-21 21:59:07 -0800

    remove text receiving

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,17 +14,5 @@ class MessagesController < ApplicationController
       end
     end
     redirect_to root_path
-  end
-
-  def receive_texts    
-    @phone_number = params[:From]
-    @body = if params[:Body].nil? then '' else params[:Body].downcase end
-    if @body.blank?
-      respond('Sorry No reply was received, please try again')
-    else
-      @participant = Participant.find_by(number: @phone_number)
-      process_message(@participant, @body)
-    end
-    render nothing: true
-  end
+  end  
 end
