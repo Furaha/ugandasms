c528b527258967d1a5d20ba8831b268689868af1
Arafat M <github@arafatm.com>
2016-03-05 19:11:53 -0600

    redirec on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      # Create an error message.
+      flash[:danger] = 'Invalid login'
+      render 'new'
+    end
   end
 
   def destroy
