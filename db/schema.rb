8fc4ad62a2822cabd7efa101edb713b74cc89b26
Arafat M <github@arafatm.com>
2016-03-05 18:07:02 -0600

    migrate user index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160305235352) do
+ActiveRecord::Schema.define(version: 20160306000102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,5 +53,7 @@ ActiveRecord::Schema.define(version: 20160305235352) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
 
 end
