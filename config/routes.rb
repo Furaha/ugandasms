c4b5d97c58e945bdceb21d5318f3a18df62805c2
Arafat M <github@arafatm.com>
2016-03-06 01:36:46 -0600

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  resources :regions
   get 'users/new'
 
-  root 'campaigns#index'
+  root 'regions#index'
+
+  resources :regions
 
   get    'login' => 'sessions#new'
   post   'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
-  resources :campaigns
-
-  match '/start_campaign', to: 'messages#start_campaign', via: :post, as: :start_campaign
-  match 'send_question', to: 'messages#send_question', via: :post, as: :send_question
-  match '/receive_texts', to: 'messages#receive_texts', via: :post, as: :receive_texts
-
   resources :participants
+
+  #resources :campaigns
+
+  #match '/start_campaign', to: 'messages#start_campaign', via: :post, as: :start_campaign
+  #match 'send_question', to: 'messages#send_question', via: :post, as: :send_question
+  #match '/receive_texts', to: 'messages#receive_texts', via: :post, as: :receive_texts
+
 
 end
