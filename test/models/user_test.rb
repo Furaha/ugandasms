5a53340a3364037ad89f8c853b3a97fc2e62e3ef
Arafat M <github@arafatm.com>
2016-03-05 18:01:17 -0600

    User validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(name: "Adam")
+  end
+
+  test "should be valid" do 
+    assert @user.valid?
+  end
+
+  test "requires a name" do 
+    @user.name = nil
+    assert_not @user.valid?
+  end
+
+  test "should be unique" do
+    dupu = @user.dup
+    @user.save
+    assert_not dupu.valid?
+  end
 end
