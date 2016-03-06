db87530cd9f8d853f9d4b7988a7c16f91d2e37a1
Arafat M <github@arafatm.com>
2016-03-05 18:46:05 -0600

    Sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root 'campaigns#index'
+
+  get    'login' => 'sessions#new'
+  post   'login' => 'sessions#create'
+  delete 'login' => 'sessions#destroy'
+
   resources :campaigns
+
   match '/start_campaign', to: 'messages#start_campaign', via: :post, as: :start_campaign
   match 'send_question', to: 'messages#send_question', via: :post, as: :send_question
   match '/receive_texts', to: 'messages#receive_texts', via: :post, as: :receive_texts
+
   resources :participants
+
 end
