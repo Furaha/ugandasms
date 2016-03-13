8e7338f6117c74c356b1290b775b9de97bdc1197
Arafat M <github@arafatm.com>
2016-03-13 15:51:52 -0500

    style messages

--- app/assets/javascripts/programs.js
+++ app/assets/javascripts/programs.js
@@ -16,22 +16,29 @@ function addMessageField() {
 
   //create <div> tag
   var message = document.createElement("div");
+  message.setAttribute("class", "form-group");
 
   //create label for Title, set it's for attribute, 
   //and append it to <div> element
   var labelTitle = document.createElement("label");
   labelTitle.setAttribute("for", idAttributTitle);
-  var titleLabelText = document.createTextNode("Title");
+  labelTitle.setAttribute("class", idAttributTitle);
+  labelTitle.setAttribute("class", "col-sm-2 control-label");
+  var titleLabelText = document.createTextNode("Message");
   labelTitle.appendChild(titleLabelText);
   message.appendChild(labelTitle);
 
   //create input for Title, set it's type, id and name attribute, 
   //and append it to <div> element
+  var messageDiv = document.createElement("div");
+  messageDiv.setAttribute("class", "col-sm-10");
   var inputTitle = document.createElement("INPUT");
   inputTitle.setAttribute("type", "text");
   inputTitle.setAttribute("id", idAttributTitle);
   inputTitle.setAttribute("name", nameAttributTitle);
-  message.appendChild(inputTitle);
+  inputTitle.setAttribute("class", "form-control");
+  messageDiv.appendChild(inputTitle);
+  message.appendChild(messageDiv);
 
   //add created <div> element with its child elements 
   //(label and input) to myList (<ul>) element
