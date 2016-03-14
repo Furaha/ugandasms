801361db7d7a2afee7f8a70eab94b7a797ce7c39
Arafat M <github@arafatm.com>
2016-03-13 20:14:21 -0500

    program reject message/recipient if blank

--- app/models/program.rb
+++ app/models/program.rb
@@ -6,6 +6,8 @@ class Program < ActiveRecord::Base
   has_many :messages
   has_many :recipients
 
-  accepts_nested_attributes_for :messages, allow_destroy: true
-  accepts_nested_attributes_for :recipients, allow_destroy: true
+  accepts_nested_attributes_for :messages, allow_destroy: true, 
+    reject_if: proc { |attributes| attributes['title'].blank?   }
+  accepts_nested_attributes_for :recipients, allow_destroy: true,
+    reject_if: proc { |attributes| attributes['name'].blank? || attributes['number'].blank?   }
 end
