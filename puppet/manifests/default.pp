924a7ca20a962aa6b05cf5eaaa318d447bb61486
Arafat M <github@arafatm.com>
2016-03-19 23:47:03 -0700

    install bundler

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -3,14 +3,22 @@ include apt
 user { 'deploy':
   ensure => 'present',
   groups => 'sudo',
-}
+} ->
 
 package { 'software-properties-common': ensure => latest, } ->
+
 apt::ppa { 'ppa:brightbox/ruby-ng': } ->
+
 exec { 'apt-get update':
   command => "/usr/bin/apt-get update",
   onlyif => "/bin/bash -c 'exit $(( $(( $(date +%s) - $(stat -c %Y /var/lib/apt/lists/$( ls /var/lib/apt/lists/ -tr1|tail -1 )) )) <= 604800 ))'"
 } ->
-package { 'ruby2.3': ensure => present, }
+
+package { 'ruby2.3': ensure => present, } ->
+
+exec { 'gems': 
+  command => "gem install bundler",
+  user    => "deploy",
+}
 
 
